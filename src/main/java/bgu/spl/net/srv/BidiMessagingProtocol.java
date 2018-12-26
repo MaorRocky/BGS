b6af19ff0452a2c7d5fc62685d59de5daeaad558
maorrocky <maorrocky@gmail.com>
2018-12-26 17:56:46 +0200

    Merge branch 'master' of https://github.com/MaorRocky/BGS
    # Conflicts:
    #	src/main/java/bgu/spl/net/srv/messages/NotificationMessage.java

---
+++ src/main/java/bgu/spl/net/srv/BidiMessagingProtocol.java
@@ -0,0 +1,60 @@
+package bgu.spl.net.srv;
+
+import bgu.spl.net.api.bidi.*;
+import bgu.spl.net.srv.messages.Message;
+
+import java.io.UncheckedIOException;
+
+public class BidiMessagingProtocol implements bgu.spl.net.api.bidi.BidiMessagingProtocol {
+    private int connectionId;
+    private Connections connections;
+
+
+    @Override
+    public void start(int connectionId, Connections connections) {
+        this.connectionId = connectionId;
+        this.connections = connections;
+    }
+
+    @Override
+    public void process(Object message) {
+        Message msg = (Message) message;
+        String messageType = msg.getType();
+        switch (messageType) {
+            case "RegisterMessage":
+                break;
+            case "LoginMessage":
+                break;
+            case "LogoutMessage":
+                break;
+            case "FollowMessage":
+                break;
+            case "PostMessage":
+                break;
+            case "PrivateMessage":
+                break;
+            case "UserListMessage":
+                break;
+            case "StatMessage":
+                break;
+        }
+
+    }
+
+    @Override
+    public boolean shouldTerminate() {
+        return false;
+    }
+
+
+    private void RegisterMessage() {
+    }
+
+    public int getConnectionId() {
+        return connectionId;
+    }
+
+    public Connections getConnections() {
+        return connections;
+    }
+}
