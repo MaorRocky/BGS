01f48f119a230c8a3deb3ef65bdb6f9b1e7494a6
maorrocky <maorrocky@gmail.com>
2018-12-26 18:16:02 +0200

    Merge branch 'master' of https://github.com/MaorRocky/BGS
    # Conflicts:
    #	src/main/java/bgu/spl/net/srv/MessageEncoderDecoderImpl.java

--- src/main/java/bgu/spl/net/srv/MessageEncoderDecoderImpl.java
+++ src/main/java/bgu/spl/net/srv/MessageEncoderDecoderImpl.java
@@ -122,7 +122,6 @@ public class MessageEncoderDecoderImpl implements MessageEncoderDecoder<Message>
     }
 
 
-
     private short bytesToShort(byte[] byteArr)  {
         short result = (short)((byteArr[0] & 0xff) << 8);
         result += (short)(byteArr[1] & 0xff);
--- src/main/java/bgu/spl/net/srv/messages/NotificationMessage.java
+++ src/main/java/bgu/spl/net/srv/messages/NotificationMessage.java
@@ -23,5 +23,4 @@ public class NotificationMessage extends Message {
         return content;
     }
 
-
 }
