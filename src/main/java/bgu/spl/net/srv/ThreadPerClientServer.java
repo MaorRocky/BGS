ac6ab3850040c87376319f78cd7415f56929cac5
MaorRocky <maorrocky@gmail.com>
2018-12-29 18:21:04 +0200

    Merge branch 'master' of https://github.com/MaorRocky/BGS
    # Conflicts:
    #	src/main/java/bgu/spl/net/srv/BidiMessagingProtocolImpl.java
    #	src/main/java/bgu/spl/net/srv/ConnectionsImpl.java

--- src/main/java/bgu/spl/net/srv/ThreadPerClientServer.java
+++ src/main/java/bgu/spl/net/srv/ThreadPerClientServer.java
@@ -1,23 +1,15 @@
-import bgu.spl.net.api.MessageEncoderDecoder;
-import bgu.spl.net.api.MessagingProtocol;
-import bgu.spl.net.srv.BaseServer;
-import bgu.spl.net.srv.bidi.ConnectionHandler;
+package bgu.spl.net.srv;
 
 import java.util.function.Supplier;
 
 public class ThreadPerClientServer extends BaseServer {
- 
-    public ThreadPerClientServer(
-            int port,
-            Supplier<MessagingProtocol> protocolFactory,
-            Supplier<MessageEncoderDecoder> encoderDecoderFactory) {
- 
-        super(port, protocolFactory, encoderDecoderFactory);
+
+    public ThreadPerClientServer(int port, Supplier protocolFactory, Supplier encdecFactory) {
+        super(port, protocolFactory, encdecFactory);
     }
- 
+
     @Override
-    protected void execute(ConnectionHandler handler) {
+    protected void execute(BlockingConnectionHandler handler) {
         new Thread(handler).start();
     }
- 
 }
