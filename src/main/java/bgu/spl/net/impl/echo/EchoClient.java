d084812b66ddbb581c30f6aa0078d218bc76dcab
maorrocky <maorrocky@gmail.com>
2019-01-01 15:54:12 +0200

    Merge branch 'master' of https://github.com/MaorRocky/BGS
    # Conflicts:
    #	src/main/java/bgu/spl/net/srv/MessageEncoderDecoderImpl.java

--- src/main/java/bgu/spl/net/impl/echo/EchoClient.java
+++ src/main/java/bgu/spl/net/impl/echo/EchoClient.java
@@ -1,11 +1,5 @@
 package bgu.spl.net.impl.echo;
 
-import bgu.spl.net.api.bidi.BidiMessagingProtocol;
-import bgu.spl.net.srv.BidiMessagingProtocolImpl;
-import bgu.spl.net.srv.DataBase;
-import bgu.spl.net.srv.MessageEncoderDecoderImpl;
-import bgu.spl.net.srv.Server;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -17,10 +11,28 @@ public class EchoClient {
 
     public static void main(String[] args) throws IOException {
 
-        //DataBase dataBase = new DataBase();
-        Server.threadPerClient(7777,
-                () -> new BidiMessagingProtocolImpl(),
-                () -> new MessageEncoderDecoderImpl()
-        ).serve();
+        if (args.length == 0) {
+            args = new String[]{"localhost", "hello"};
+        }
+
+        if (args.length < 2) {
+            System.out.println("you must supply two arguments: host, message");
+            System.exit(1);
+        }
+
+        //BufferedReader and BufferedWriter automatically using UTF-8 encoding
+        try (Socket sock = new Socket(args[0], 7777);
+                BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()))) {
+
+            System.out.println("sending message to server");
+            out.write(args[1]);
+            out.newLine();
+            out.flush();
+
+            System.out.println("awaiting response");
+            String line = in.readLine();
+            System.out.println("message from server: " + line);
+        }
     }
 }
