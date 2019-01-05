b8d2d0fcf9b38430909d93f1868cfbcc03db10a4
maorrocky <maorrocky@gmail.com>
2019-01-05 19:14:15 +0200

    Merge branch 'master' of https://github.com/MaorRocky/BGS
    # Conflicts:
    #	src/main/java/bgu/spl/net/srv/MessageEncoderDecoderImpl.java

--- src/main/java/bgu/spl/net/impl/BGSServer/TPCMain.java
+++ src/main/java/bgu/spl/net/impl/BGSServer/TPCMain.java
@@ -1,11 +1,11 @@
-package bgu.spl.net.impl.bidi;
+package bgu.spl.net.impl.BGSServer;
 
 import bgu.spl.net.srv.BidiMessagingProtocolImpl;
 import bgu.spl.net.srv.DataBase;
 import bgu.spl.net.srv.MessageEncoderDecoderImpl;
 import bgu.spl.net.srv.Server;
 
-public class main {
+public class TPCMain {
     public static void main(String[] args) {
 
         DataBase dataBase = new DataBase();
